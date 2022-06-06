793c5bce7bc8d54e76aba1bf4a2cc69fe258a54d
Jason Shannon <jshannon3343@gmail.com>
2022-06-06 13:19:23 -0500

    fitting general layout

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+//key
+const apiKey = '78d1587d85cd82464e06a60cc926a779';
+
+//example lat, lon, part
+let lat = '33.44';
+let lon = '94.04';
+let part = 'hourly, daily';
+
+//api key name
+let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
+
+
+
+
+
