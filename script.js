3c0aba2a168d6efe54db25616a82d3d2f9280b8d
Jason Shannon <jshannon3343@gmail.com>
2022-06-07 14:16:52 -0500

    update

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 
+let cityName = document.getElementById('searchBox');
+let submitBtn = document.getElementById('submitSearch');
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log(cityName.value);
+   
+})
+
+
 
 //key
 const apiKey = '';
@@ -8,10 +18,10 @@ let lat = '33.44';
 let lon = '94.04';
 let part = 'hourly, daily';
 
-//api key name
-let api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
+//api links
+// let geocoder = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=${limit}&appid=${APIkey}`;
 
 
-
+// let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
 
