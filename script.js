41634fd80f7325c5ba3f6ab69126d4249cb92ea8
Jason Shannon <jshannon3343@gmail.com>
2022-06-15 00:09:40 -0500

    update

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 
-let cityName = document.getElementById('searchBox');
 let submitBtn = document.getElementById('submitSearch');
+
 
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    let cityName = document.getElementById('searchBox');
     console.log(cityName.value);
-   
+
+    const apiKey = '';
+    let geocoder = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
 })
 
 
 
 //key
-const apiKey = '';
+
 
 //example lat, lon, part
 let lat = '33.44';
 let lon = '94.04';
 let part = 'hourly, daily';
 
-//api links
-// let geocoder = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=${limit}&appid=${APIkey}`;
+let cityname = 'chicago';
 
 
-// let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
 
 
+
+let currentWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+
+
+let cityName = document.getElementById('searchBox');
+const apiKey = '';
+
+let requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`;
+
+
+fetch(requestURL)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
